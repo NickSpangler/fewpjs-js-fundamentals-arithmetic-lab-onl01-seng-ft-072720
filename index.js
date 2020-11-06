61f36b78ae62d6993621f10679fcced758d95e20
NickSpangler <Spangleyman@mac.com>
2020-11-06 15:40:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 1
+const num2 = 62
 
-// Write your code here
+const multiply = num1*num2
+
+const random = Math.random() * 10 + 1
+
+const  mod = 14 % 10
+
+const max = Math.max(1, 2, 20, 4, 5)
